b79c2057110ea96bbab824352ab332297d024220
talspektor <talspektor@gmail.com>
2020-09-28 23:02:31 +0300

    Test git ignore

---
+++ app/src/main/java/com/talspektor/finedplaces/Keys.kt
@@ -0,0 +1,8 @@
+package com.talspektor.finedplaces
+
+class Keys {
+    companion object {
+        const val GOOGLE_PLACES_KEY = "AIzaSyB_N8M_pmx6D5u0nSdGUG2AdibZm8Mk0JY"
+        const val TEST = ""
+    }
+}
